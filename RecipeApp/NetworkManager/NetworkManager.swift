c4c316feae5e58bfb2e84b6d8cf53c5c67cf0927
arpit5492 <Arpit.Mallick@techconsulting.tech>
2024-10-18 12:00:36 -0400

    Updated Network manager

--- RecipeApp/NetworkManager/NetworkManager.swift
+++ RecipeApp/NetworkManager/NetworkManager.swift
@@ -7,12 +7,47 @@
 
 import Foundation
 
+enum ApiError: Error {
+    case invalidUrl
+    case invalidResponse
+    case decodingError
+    case badRequest(String) // 400 status code range
+    case serverError(String) // 500 status code range
+    case other(String)
+}
+
 protocol NetworkManagerActions {
     func apiCall<T: Decodable>(modelType: T.Type, apiUrl: String) async throws -> T
 }
 
 class NetworkManager: NetworkManagerActions {
     func apiCall<T>(modelType: T.Type, apiUrl: String) async throws -> T where T : Decodable {
-        <#code#>
+        guard let url = URL(string: apiUrl) else {
+            throw ApiError.invalidUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let _response = response as? HTTPURLResponse else {
+            throw ApiError.invalidResponse
+        }
+        
+        switch _response.statusCode {
+        case 200:
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                return decodedData
+            } catch {
+                throw ApiError.decodingError
+            }
+            
+        case 400..<500:
+            throw ApiError.badRequest("Client error: \(_response.statusCode)")
+            
+        case 500..<600:
+            throw ApiError.serverError("Internal Server Error: \(_response.statusCode)")
+            
+        default:
+            throw ApiError.other("Unexpected status code: \(_response.statusCode)")
+        }
     }
 }
